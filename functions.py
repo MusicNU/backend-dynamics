09ad9d399867956281a6ff3c1016a23d60f7a133
Acton Healy <afkhealy@gmail.com>
2025-01-28 21:43:57 -0600

    deleted useless files

--- functions.py
+++
@@ -1,8 +0,0 @@
-
-def add_five(num: int) -> int:
-    return num + 5
-
-def divider(dividend: int, divider: int) -> float:
-    if divider == 0:
-        return float('inf')
-    return dividend / divider
