df64f0aa0af0cf09debd9a87a618844573f65912
Acton Healy <afkhealy@gmail.com>
2025-01-31 14:04:36 -0600

    changed this file to actually work

---
+++ all_test.py
@@ -0,0 +1,4 @@
+from feedback import load_audio, get_dynamics, get_tempos, rms_note_by_note, analyze_performance
+
+def test_testing_that_testing_works() -> None:
+    assert "This is nothing code" == "This is nothing code"
--- tests.py
+++
@@ -1,4 +0,0 @@
-from feedback_functions import rms_note_by_note, analyze_performance
-
-def test_add_five_default() -> None:
-    assert "This is nothing code" == "This is nothing code"
